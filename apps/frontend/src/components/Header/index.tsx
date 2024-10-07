bcbf0867e2693c2541a03c66d8c5dd81dd1fa6cf
Veronika Timakova <tymnik@ukr.net>
2024-10-07 16:12:11 +0300

    add: WelcomePageContent, adaptives, styles

--- apps/frontend/src/components/Header/index.tsx
+++ apps/frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 import { ROUTES } from '../../constants/routes'
 import Icon from '../../ui/Icon'
@@ -21,12 +21,12 @@ const Header = () => {
                 <div className={style.header__menuWrapper}>
                     <Link to={ROUTES.WELCOME}><Icon id="logo" className={style.header__logo} /></Link>
                     <ul className={style.header__menu}>
-                        <li className={style.header__menu__item}> <Link to={ROUTES.ENCYCLOPEDIA}>Encyclopedia</Link></li>
-                        <li className={style.header__menu__item}> <Link to={ROUTES.DASHBOARD}>Own Collection</Link></li>
+                        <li className={style.header__menu__item}> <NavLink to={ROUTES.ENCYCLOPEDIA}>Encyclopedia</NavLink></li>
+                        <li className={style.header__menu__item}> <NavLink to={ROUTES.DASHBOARD}>Own Collection</NavLink></li>
                     </ul>
                 </div>
                 <div className={style.header__settingsWrapper}>
-                    <Link to={ROUTES.USER_SETTINGS}><Icon id="user-settings" className={style.header__icon} /></Link>
+                    <NavLink to={ROUTES.USER_SETTINGS}><Icon id="user-settings" className={style.header__icon} /></NavLink>
                     <ThemeToggle onThemeChange={handleThemeChange} />
                 </div>
             </div>
