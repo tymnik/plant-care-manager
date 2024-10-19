a5b557f90384cab05bb9837d6f26f515d131f92f
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2024-10-19 18:38:47 +0300

    added: unit tests

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@plant-care/types';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { BasePrismaCrudService } from 'src/shared/classes/BasePrismaCrudService';
 import { ICRUDService } from 'src/shared/interfaces/crud/service/ICRUD';
 
