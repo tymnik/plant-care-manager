961acdaa5c9c4e591eae31b80676d1eef11e947f
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2024-10-24 15:50:21 +0300

    Update BaseCrudController.ts

--- apps/backend/src/shared/classes/BaseCrudController.ts
+++ apps/backend/src/shared/classes/BaseCrudController.ts
@@ -26,14 +26,14 @@ export class BaseCrudController<
     const count = await this.baseCrudService.count();
 
     const data = await this.baseCrudService.findMany({
-      skip: (body.page - 1) * body.perPage,
-      take: +body.perPage,
+      skip: (body.page - 1) * body.perPage || 0,
+      take: +body.perPage || 20,
       orderBy:
         body.orderBy && body.order
           ? {
               [body.orderBy]: body.order,
             }
-          : null,
+          : { id: 'desc' },
     });
     return new Pagination<T>(data, count, body.perPage, body.page);
   }
